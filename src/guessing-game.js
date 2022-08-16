1a1d3925873789899393e429c77e185cfcde436c
Vladimir <vovchik.niko@gmail.com>
2022-08-17 00:33:23 +0300

    test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left;
+        this.right;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
+        console.log(Math.round((this.right + this.left) / 2));
+        return Math.round((this.right + this.left) / 2);
 
     }
 
     lower() {
+        this.right = Math.round((this.right + this.left) / 2);
 
     }
 
     greater() {
 
+        this.left = Math.round((this.right + this.left) / 2);
     }
 }
 
 module.exports = GuessingGame;
+
+
